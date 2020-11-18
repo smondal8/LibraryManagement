dac97b0de785ed16449442e3ee8392c4796bad70
smondal8 <54061876+smondal8@users.noreply.github.com>
2020-11-18 10:58:42 +0530

    update version

--- LibraryAngularUI/src/app/Books-Registration/book-registration.component.spec.ts
+++ LibraryAngularUI/src/app/Books-Registration/book-registration.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RegistrationBookComponent } from './book-registration.component';
 
@@ -6,7 +6,7 @@ describe('RegistrationMoviesComponent', () => {
   let component: RegistrationBookComponent;
   let fixture: ComponentFixture<RegistrationBookComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RegistrationBookComponent ]
     })
--- LibraryAngularUI/src/app/Library-list/Library-list.component.spec.ts
+++ LibraryAngularUI/src/app/Library-list/Library-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LibraryListComponent } from './Library-list.component';
 
@@ -6,7 +6,7 @@ describe('MovieListComponent', () => {
   let component: LibraryListComponent;
   let fixture: ComponentFixture<LibraryListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LibraryListComponent ]
     })
