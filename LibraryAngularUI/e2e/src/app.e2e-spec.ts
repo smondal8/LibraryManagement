b5649c824b8823adddd743a647b9d026c618ec63
smondal8 <54061876+smondal8@users.noreply.github.com>
2021-08-17 20:33:19 +0530

    added e2e tests

--- LibraryAngularUI/e2e/src/app.e2e-spec.ts
+++ LibraryAngularUI/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, element, by, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,10 +8,48 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should contain correct title message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ServiceDemo!');
+    expect(page.getTitleText()).toEqual('Welcome to Library Management System!');
   });
+  it('should contain correct Table Headers', () => {
+    page.navigateTo();    
+    expect(page.getTheHeaders()).toContain("Library Code");
+  });
+  it('should contain correct value in details table', ()=>{
+    page.navigateTo();
+    element.all(by.tagName('td')).then(function(items) {
+      expect(items.length).toBe(6);
+      expect(items[0].getText()).toBe('101');
+      expect(items[1].getText()).toBe('Mumbai');      
+      expect(items[4].getText()).toBe('Bangalore');
+      expect(items[3].getText()).toBe('102');      
+    });
+    
+  })
+  
+  it('should display book details after click of details button', ()=>{
+    page.navigateTo();
+    element.all(by.tagName('button')).then(function(buttons){
+      buttons[2].click();
+      element.all(by.css('td')).then(function(items){
+        expect(items.length).toBe(9);
+        expect(items[6].getText()).toBe('101');
+        expect(items[7].getText()).toBe('Java - Complete Reference');
+        expect(items[8].getText()).toBe('Herbert Schildt');
+      });      
+    }); 
+    element.all(by.tagName('button')).then(function(buttons){      
+      buttons[1].click();
+      element.all(by.css('td')).then(function(items){
+        expect(items.length).toBe(9);
+        expect(items[6].getText()).toBe('100');
+        expect(items[7].getText()).toBe('Let us C');
+        expect(items[8].getText()).toBe('Yashwanth Kanetkar');
+      });    
+    });    
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- LibraryAngularUI/e2e/src/app.po.ts
+++ LibraryAngularUI/e2e/src/app.po.ts
@@ -1,11 +1,18 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  gettableCoulmn() {
+    return element(by.tagName('td')).last() as Promise<any>;
+  }
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+
+  getTheHeaders() {
+    return element(by.tagName('th')).getText() as Promise<string>;
   }
 }
